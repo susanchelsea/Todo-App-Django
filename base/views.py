1bb4599749681222548f36bf5974c196c9b9ad6c
susanchelsea <susanchelsian@gmail.com>
2024-02-15 20:45:50 +0545

    Todo app

---
+++ base/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+def home(request):
+    todo_objs = Todo.objects.all()
+    data = {'todos':todo_objs}
+    return render(request, 'index.html',context=data)
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        Todo.objects.create(name=name,description=description,status=status)
+        return redirect('home')
+    return render(request, 'create.html')
+
+def edit(request,pk):
+    todo_obj = Todo.objects.get(id=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo_obj.name = name
+        todo_obj.description = description
+        todo_obj.status = status
+        todo_obj.save()
+        return redirect('home')
+    data = {'todo':todo_obj}
+    return render(request, 'edit.html',context=data)
+    
+def delete(request,pk):
+    todo_obj = Todo.objects.get(id=pk)
+    todo_obj.delete()
+    return redirect('home')
+
+def deleteall(request):
+    todo_obj = Todo.objects.all()
+    if request.method == "POST":
+        todo_obj.delete()
+        return redirect('home')
+    return render(request, 'deleteall.html')
